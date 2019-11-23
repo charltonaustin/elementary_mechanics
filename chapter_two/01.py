3b674fa1f0dfaf5c68794109b6472c1813c93d6a
charltonaustin <charlton.austin@gmail.com>
2019-11-23 13:52:01 -0500

    adding in a problem

---
+++ chapter_two/01.py
@@ -0,0 +1,14 @@
+def minutes(hours):
+    return 3600 * hours
+
+
+print("1.5 hrs in mins:")
+print(minutes(1.5))
+
+
+print("12 hrs in mins:")
+print(minutes(12))
+
+
+print("24 hrs in mins:")
+print(minutes(24))
