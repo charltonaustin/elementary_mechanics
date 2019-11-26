a990176e9cb5b97b0573b431f2b383c876f080ed
charltonaustin <charlton.austin@gmail.com>
2019-11-26 11:26:44 -0600

    ch 2 problem 6

--- chapter_two/06.py
+++ chapter_two/06.py
@@ -1,8 +1,12 @@
-from numpy import linspace
+from pylab import linspace, plot, show
 
 
-def fvalue(x, n):
-    return x ** (-n)
+def fvalue(x_, n):
+    return x_ ** (-n)
 
 
-x = linspace()
+x = linspace(-1, 1, 100)
+plot(x, fvalue(x, 1))
+plot(x, fvalue(x, 2))
+plot(x, fvalue(x, 3))
+show()
